36262137db8e051aac3e88a0988f57746a9ab45d
CodeSandbox <support@codesandbox.io>
2023-10-11 12:49:45 +0000

    Sandbox snapshot commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,12 @@
-import { getServerSession } from "next-auth";
 import { getGalleryList } from "../services/gallery";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { authOptions } from "./api/auth/[...nextauth]";
-
-// const getPosts = async () => {
-//   try {
-//     const res = await axios.get(
-//       "https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=20"
-//     );
-
-//     return res.data;
-//   } catch (error) {}
-// };
 
 const page = async () => {
-  // const session = await getServerSession(authOptions);
-  // console.log("session", session);
-
   const posts = await getGalleryList({ limit: 52, offset: 0 });
   return (
     <main className="mt-5 container mx-auto">
-      {/* <pre>session: {JSON.stringify(session)}</pre> */}
       <div className="my-5 text-3xl font-bold text-center">Photo Gallery</div>
       <div className="grid grid-cols-fluid">
         {posts?.photos.map((item, index) => {
@@ -37,8 +21,8 @@ const page = async () => {
                     className="w-full"
                     src={item.url}
                     alt={item.title}
-                    width={0}
-                    height={0}
+                    width={100}
+                    height={100}
                     sizes="100"
                   />
                   <div className="p-4">
